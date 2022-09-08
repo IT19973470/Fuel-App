a793017ba4d3ffbef13beeb7bd6fa5fe7498f47c
IT19973470 <imalkagunawardana1@gmail.com>
2022-09-08 22:08:39 +0530

    up

---
+++ Frontend/src/app/content/fuel-station/fuel-availability/fuel-availability.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fuel-availability',
+  templateUrl: './fuel-availability.component.html',
+  styleUrls: ['./fuel-availability.component.css']
+})
+export class FuelAvailabilityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
